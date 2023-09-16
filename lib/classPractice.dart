530f7c276367383541d3f4fba428e460e689ddc4
saikat111 <s8856010@gmail.com>
2023-09-17 03:51:13 +0600

    practice variable

---
+++ lib/classPractice.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main(){
+  print("hello");
+  stdout.write("object");
+
+  var name = stdin.readLineSync();
+  print("Your name, $name");
+  new Human();
+}
+
+
+
+class Human{
+  Human();
+  
+}
---
+++ lib/variable.dart
@@ -0,0 +1,6 @@
+void main(){
+  int? a;//can't be null
+  a = 5;
+  print(a);
+}
+
